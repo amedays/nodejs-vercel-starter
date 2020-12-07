d0a1bf85119c6a4c000f9e8aa449a0c40bfee25c
amedays <72970432+amedays@users.noreply.github.com>
2020-12-08 00:29:37 +0800

    Update app.js
    feat: redirect
    
    fix: uppercase
    
    temp

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const { domain } = require('process')
+const url = require('url')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// [{"hostname":"baidu.com","protocol":"https:"}]
+let server = process.env.SERVER
+let domains = Json.parse(server)
 
+let number = domains.length
+app.get('*', (req, res) => {
+    var date = new Date();
+    var hour = date.getHours();
+    let gap = 24/number;
+    let index =  Math.floor(hour / gap);
+    let { path } = url.parse(req.url);
+    var distination = domains[index];
+    distination['pathname'] = path;
+    console.log(url.format(distination));
+    res.redirect(302, url.format(distination));
+})
 module.exports = app
